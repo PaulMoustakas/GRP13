7c9f22c9a1f19eb985c482eaabf05839195d0c2f
PaulMoustakas <paul.moustakas@me.com>
2020-12-29 19:17:49 +0100

    StartTest
    TESTING TESTING

---
+++ BackEnd-V.6.0.0/src/main/java/APIRunner.java
@@ -0,0 +1,35 @@
+
+import spark.ModelAndView;
+import spark.template.pebble.PebbleTemplateEngine;
+
+import static spark.Spark.*;
+
+/**
+ * Proj X - API mashup
+ * Spotify <--> Google maps
+ * @author GRP 13
+ * @version 2.0.0
+ */
+
+public class APIRunner {
+    public static void main(String[] args) {
+        port(3000);
+        staticFiles.location("/public");
+
+
+        get("/front", (request, response) -> {
+            return new PebbleTemplateEngine().render (
+                    new ModelAndView(null, "/templates/"));
+        });
+
+
+
+
+        get("/hello", (req, res)->"Hello, world");
+
+        get("/hello/:name", (req,res)->{
+            return "Hello, "+ req.params(":name");
+        });
+    }
+}
+
